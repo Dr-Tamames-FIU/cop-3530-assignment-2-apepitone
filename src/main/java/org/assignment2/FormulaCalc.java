1a812ae72ad26c8dc2de04a1672bab7c6044dd6f
apepitone <apepi004@fiu.edu>
2023-09-24 18:53:35 -0400

    .

--- src/main/java/org/assignment2/FormulaCalc.java
+++ src/main/java/org/assignment2/FormulaCalc.java
@@ -1,116 +1,77 @@
 package org.assignment2;
 
+
+import java.util.Stack;
+
 public class FormulaCalc {
 
-    public static void main(String[] args)
-    {
-
+    private static int getAtomicNumber(String element) {
+        switch (element) {
+            case "H": return 1;
+            case "C": return 6;
+            case "O": return 8;
+            case "N": return 7;
+            case "K": return 19;
+            case "Na": return 11;
+            case "Cl": return 17;
+            case "Br": return 35;
+            default: return -1; // Invalid element
+        }
     }
-    /**
-     * Algorithm
-     * @param a Molecular formula
-     * @return Total number of protons
-     */
-    public static int Algorithm(String a)
-    {
-        
-       /* Use the class exercise for Lecture 6 and particularly, the answers on:
-          https://itamames.github.io/Lecture6Answers/
-          You can see how to parse an array in there.
-          Once you are dealing character by character, you can set a multiple if statement to check if that character is a capital letter, a small case letter, a number, or a parenthesis.
-          For each of these 4 options, you need to perform a decision.
 
-        //If is a capital letter 
-        {
-            //if the stack is not empty
-            {
-            //pop 1:
-            //add it to cumulative sum
+    public static int Algorithm(String a) {
+        Stack<Integer> stack = new Stack<>();
+        int protons = 0;
+
+        for (int i = 0; i < a.length(); i++) {
+            char c = a.charAt(i);
+            if (Character.isUpperCase(c)) {
+                StringBuilder element = new StringBuilder();
+                element.append(c);
+                while (i + 1 < a.length() && Character.isLowerCase(a.charAt(i + 1))) {
+                    element.append(a.charAt(i + 1));
+                    i++;
+                }
+                int atomicNumber = getAtomicNumber(element.toString());
+                stack.push(atomicNumber);
+            } else if (Character.isDigit(c)) {
+                int count = c - '0';
+                if (i + 1 < a.length() && Character.isDigit(a.charAt(i + 1))) {
+                    count = count * 10 + (a.charAt(i + 1) - '0');
+                    i++;
+                }
+                int top = stack.pop();
+                stack.push(top * count);
+            } else if (c == '(') {
+                stack.push(-1); // Use -1 to mark the start of a subexpression
+            } else if (c == ')') {
+                int subTotal = 0;
+                while (stack.peek() != -1) {
+                    subTotal += stack.pop();
+                }
+                stack.pop(); // Pop the -1 marker
+                stack.push(subTotal);
             }
-
-            //request the string value and add it to stack.
-        }
-        //else if is a lowercase letter
-        {
-            //pop stack to clear it.
-            //grab the prior letter and request the string value and add it to stack
-        }
-        //else if it is a number
-        {
-            //pop 1:
-            //multiply times the number
-            //add it to a cumulative sum
-        }
-        //else if parenthesis
-        { 
-
         }
 
-      return 1;
-  }
+        while (!stack.isEmpty()) {
+            protons += stack.pop();
+        }
 
-    
-  private static int getElementProtons(String c) 
-  {
-    
-    switch (c) 
-    {
-    case "H": return 1;
-    case "He": return 2;
-    case "Li": return 3;
-    case "Be": return 4;
-    case "B": return 5;
-    case "C": return 6;
-    case "N": return 7;
-    case "O": return 8;
-    case "F": return 9;
-    case "Ne": return 10;
-    case "Na": return 11;
-    case "Mg": return 12;
-    case "Al": return 13;
-    case "Si": return 14;
-    case "P": return 15;
-    case "S": return 16;
-    case "Cl": return 17;
-    case "K": return 19;
-    case "Ar": return 18;
-    case "Ca": return 20;
-    case "Sc": return 21;
-    case "Ti": return 22;
-    case "V": return 23;
-    case "Cr": return 24;
-    case "Mn": return 25;
-     case "Fe": return 26;
-     case "Ni": return 28;
-     case "Co": return 27;
-     case "Cu": return 29;
-     case "Zn": return 30;
-     case "Ga": return 31;
-     case "Ge": return 32;
-     case "As": return 33;
-     case "Se": return 34;
-     case "Br": return 35;
-     case "Kr": return 36;
-     case "Rb": return 37;
-     case "Sr": return 38;
-     case "Y": return 39;
-     case "Zr": return 40;
-     case "Nb": return 41;
-     case "Mo": return 42;
-     case "Tc": return 43;
-     case "Ru": return 44;
-     case "Rh": return 45;
-     case "Pd": return 46;
-     case "Ag": return 47;
-     case "Cd": return 48;
-     case "In": return 49;
-     case "Sn": return 50;
-     case "Sb": return 51;
-     case "I": return 53;
-     case "Te": return 52;
-     case "Xe": return 54;
-     default: return 0;
+        return protons;
     }
-  }
-}
 
+    public static void main(String[] args) {
+        String[] formulas = {
+            "H",
+            "KBr",
+            "H2O",
+            "NaCl",
+            "C6H12O6"
+        };
+
+        for (String formula : formulas) {
+            System.out.println(Algorithm(formula));
+        }
+    }
+}
